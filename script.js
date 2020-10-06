5455f1f78de5776114d6e5ad4ab1b7e3e8474794
Lipton Zuma <solomonboateng231@gmail.com>
2020-10-06 04:51:12 +0000

    Update script.js

--- script.js
+++ script.js
@@ -4,13 +4,17 @@ const displayTime = () => {
         hr = date.getHours(),
         min = date.getMinutes(),
         sec = date.getSeconds();
+
     let text;
     if (hr === 0) {
         hr = 12
-        text = "AM"
+        text = 'AM'
     };
+    if (hr < 12) {
+        text = 'AM'
+    }
     if (hr > 12) {
-        hr = hr - 12
+        hr = hr - 12;
         text = "PM"
     }
 
